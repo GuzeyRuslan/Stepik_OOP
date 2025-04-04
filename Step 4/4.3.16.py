70d11c611df1d9d12ab6637ce389be6cf2c21ed0
Guzey Ruslan < @adinfra.local>
2025-04-04 16:14:44 +0500

    develop-start-4.3.16.py

---
+++ Step 4/4.3.16.py
@@ -0,0 +1,18 @@
+class Knight():
+    def __init__(self, horizontal, vertical, color):
+        self.horizontal = horizontal
+        self.vertical = vertical
+        self.color = color
+
+    def get_char(self):
+        return 'N'
+
+    def can_move(self, h, v):
+
+
+    def move_to(self):
+        pass
+
+    def draw_board(self):
+        pass
+
